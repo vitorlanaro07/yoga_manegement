de0ef352d15965f7d6704c5cf276395db35b0cf0
Vitor S. Lanaro <vitor.lanaro@hotmail.com>
2024-03-26 15:50:53 -0300

    adding buttons to edit and remove student with opacity effect and warning message

--- public/script/studentRead.js
+++ public/script/studentRead.js
@@ -1,22 +1,58 @@
+//getting the student
 function getId(element){
     let idStudent = element.children[0].innerHTML
     window.location.href = '/student/' + idStudent;
 }
 
+//buttons edit and delete
+const btnEdit = document.querySelector('.button-edit');
+const btnDelete = document.querySelector('.button-delete');
+const btnNegate = document.querySelector('.warning-no');
+const btnConfirm = document.querySelector('.warning-yes');
 
+
+editingStudent = () => {
+    console.log('editing');
+}
+
+openingWarning = () => {
+    const backgroundChange = document.querySelector('.container-opacity');
+    const boxWarning = document.querySelector('.box-warning');
+
+    backgroundChange.style.display = "block";
+    boxWarning.style.display = "flex";
+
+    btnNegate.addEventListener('click', () => {
+        backgroundChange.style.display = "none";
+        boxWarning.style.display = "none";
+    });
+
+    btnConfirm.addEventListener('click', () => {
+        window.location.href = '/students'
+    })
+}
+
+
+btnEdit.addEventListener('click', editingStudent);
+btnDelete.addEventListener('click', openingWarning);
+
+
+//checking the chekbox to open text if there is.
 const checkContainer = [...document.querySelectorAll('.has-check')];
 
 checkContainer.forEach(div => {
-    let checkBoxes = [...div.children[0].children[1].children];
-    let textAreaChild = div.children[1];
+    let option = {
+        checkBoxes: [...div.children[0].children[1].children],
+        textAreaChild: div.children[1]
+    } 
     
-    checkBoxes.forEach(check => {
+    option.checkBoxes.forEach(check => {
         if (check.tagName !== "LABEL"){
             if(check.classList.contains('checkYes')){
                 if(check.checked === true){
-                    textAreaChild.hidden = false;
+                    option.textAreaChild.hidden = false;
                 }    else {
-                    textAreaChild.hidden = true;
+                    option.textAreaChild.hidden = true;
                 }
             }
             
